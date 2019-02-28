ad1d9de15d35d37e12af36c3e347592d03ef315d
Sergio Liberman Bronfman <sliberman@alges.cl>
2019-02-28 19:58:02 +0100

    WIP

--- src/Score.java
+++ src/Score.java
@@ -30,4 +30,34 @@ public class Score {
         Arrays.sort(scores);
         return scores[0];
     }
+
+    public int score(Slide s1, Slide s2) {
+        Integer[] tags_p1 = s1.tags.toArray(new Integer[0]);
+        Integer[] tags_p2 = s2.tags.toArray(new Integer[0]);
+//        int common_tags = 0;
+//        int tags_in_p1_not_in_p2 = 0;
+//        int tags_in_p2_not_in_p1 = 0;
+        Integer[] scores = new Integer[3];
+        for (int i = 0; i < tags_p1.length; i++) {
+            for (int j = 0; j < tags_p2.length; j++) {
+                if (tags_p1[i] < tags_p2[j]) {
+                    i++;
+                    //tags_in_p1_not_in_p2++;
+                    scores[1]++;
+                } else if (tags_p1[i] > tags_p2[i]) {
+                    j++;
+                    //tags_in_p2_not_in_p1++;
+                    scores[2]++;
+                } else {
+                    // They are equal
+                    //common_tags++;
+                    scores[0]++;
+                    i++;
+                    j++;
+                }
+            }
+        }
+        Arrays.sort(scores);
+        return scores[0];
+    }
 }
