62cf7ba18d2b070221364034ac721df2cace185e
Xemnas0 <franuzzo966@gmail.com>
2019-02-28 18:52:02 +0100

    problem files added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        String[] input_filename = {"./problem_min.txt", "./another_problem.txt"};
+        String[] input_filename = {
+            "./a_example.txt",
+            "./b_lovely_landscapes.txt",
+            "c_memorable_moments.txt",
+            "d_pet_pictures.txt",
+            "e_shiny_selfies.txt"
+        };
         Solver solver = new Solver(input_filename[0]);
     }
 }
