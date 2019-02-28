62edd390ef09b8011b7b4ae9d604736f33f3bb79
Xemnas0 <franuzzo966@gmail.com>
2019-02-28 19:16:06 +0100

    building photo

--- src/Photo.java
+++ src/Photo.java
@@ -1,18 +1,31 @@
+import java.util.Hashtable;
+import java.util.LinkedList;
+
 public class Photo {
 
     boolean isHorizontal;
+    int[] tags;
 
-    public Photo(String info) {
+    public Photo(String info, LinkedList<String> categories, Hashtable<String, Boolean> inserted_categories) {
         String[] splitted_infos = info.split(" ");
+
         if (splitted_infos[0].compareTo("H") == 0){
             isHorizontal = true;
         } else {
             isHorizontal = false;
         }
 
-//        for (int i = 2; i < ; i++) {
-//
-//        }
-        System.err.println();
+        int n_tags = Integer.valueOf(splitted_infos[1]);
+        tags = new int[n_tags];
+
+        Integer tag;
+        for (int i = 2; i < splitted_infos.length; i++) {
+//            inserted_categories.get(splitted_infos[i]);
+//            if (){
+
+                continue;
+//            }
+
+        }
     }
 }
--- src/Solver.java
+++ src/Solver.java
@@ -1,10 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Hashtable;
+import java.util.LinkedList;
 
 public class Solver {
 
     int n_photos;
     Photo[] photos;
+    LinkedList<String> categories = new LinkedList<>();
+    Hashtable<String, Boolean> inserted_categories = new Hashtable<>();
 
     public Solver(String filepath) {
         try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
@@ -16,8 +20,8 @@ public class Solver {
             int index_photo = 0;
             while (line != null) {
                 line = br.readLine();
-                photos[index_photo] = new Photo(line);
-                
+                photos[index_photo] = new Photo(line, categories, inserted_categories);
+
                     ++index_photo;
             }
         } catch (Exception e) {
